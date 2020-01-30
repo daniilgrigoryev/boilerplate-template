4804d73239fbe01f77d4d9ead711d1e551fe49dc
Daniil Grigoryev <dgrigoryev@igatc.com>
2020-01-30 16:27:28 +0300

    New Benach

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode.indexOf('production') !== -1 // npm run build -> webpack --mode=production
+  const dev = argv.mode.indexOf('development') !== -1
 
   return {
-    mode: 'production',
+    mode: 'none',
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: '[name].[contenthash].js', // авторские файлы
-      publicPath: isProduction ? undefined : '/assets/',
+      publicPath: dev ? '/assets/' : undefined,
     },
     module: {
       rules: [
